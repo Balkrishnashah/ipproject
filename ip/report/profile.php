5f7a9686959d6f5cd59a98e24cbd420fa860d9f3
BalkrishnaShah <shahkrishna493@gmail.com>
2019-10-22 11:04:35 +0530

    first commit

---
+++ ip/report/profile.php
@@ -0,0 +1,378 @@
+<html><head>
+        <title>Aldel Information System</title>
+     <link rel="stylesheet" href="css/menu_style.css" type="text/css">
+       <script language="javascript" type="text/javascript">
+            function confirmation() {
+                var answer = confirm("Are you sure you want to Logout ?")
+                if (answer){
+                    window.location = "logout.php";
+                }
+            }
+        </script>
+  
+</head>
+     <!-- <body align="center" onload="noBack();" onpageshow="if (event.persisted) noBack();" onunload="">
+
+                    <ul id="css3menu1" class="topmenu">
+                                    <li class="topmenu"><a href="#"><span> Others </span></a>
+                        <ul class="pureCssMenum">
+                            <li class="topmenu"><a href="/student/view_profile.php "> View Profile </a></li>
+                                                        </ul>
+                                                </li><li class="topmenu"><a href="#"><span> Reports </span></a>
+                        <ul class="pureCssMenum">
+                            <li class="topmenu"><a href="/student/attendance_report.php "> Attendance </a></li>
+                                        </ul> -->
+<!--        <li class="topmenu"><a href="../about.php">About MIS</a></li>
+        </li><li class="toplast"><a href="javascript:void(0)" onclick="confirmation()">Logout</a></li>
+    </ul>
+    <h5 style="float: right;">Logged in as : 117mahesh4059</h5><br><br>
+ -->
+
+
+
+    <title>Student Profile</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+        <link href="css/table1.css" rel="stylesheet" type="text/css" media="screen">
+    
+    
+        <br><br>
+                    <center>
+                <table>
+                    <thead>
+                        <tr class="odd">
+                            <th scope="col" colspan="4">Personal Details</th>
+                        </tr>
+                    </thead>
+                    <tbody><tr>
+                        <td class="column1">Student PID</td><td>EU1174059</td>
+                        <td class="column1">Admission Academic Year</td><td>2017</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Branch</td>
+                        <td>
+                            IT                        </td>
+                        <td class="column1">Current Semester</td><td>5</td>
+                    </tr>
+					<tr> <!-- added on 13 july 2013 shwetali -->
+						<td class="column1">Division</td><td>A                                                      <!-- added on 12 dec 13 sharvari to display sem1 division -->
+                                                                    
+                                                
+                                                </td>
+						<td class="column1">Roll No</td><td>70</td>
+					</tr> <!-- end  -->
+                    <tr>
+                        <td class="column1">Name</td><td colspan="2">Shah Balkrishna A </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Date of Birth</td>
+                        <td>
+                            11 - 
+                            April - 
+                            2000                        </td>
+                        <td class="column1">Gender</td><td>Male</td>
+                    </tr>
+                    <tr>
+                        <td class="column1" rowspan="2">Residential Address</td><td>OM SHIV COMPLEX, B/107,</td>
+                        <td class="column1" rowspan="2">Native Address</td><td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">City</td><td>BHAYANDER</td>
+                        <td class="column1">City</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">State</td>
+                        <td>
+                                                    </td>
+                        <td class="column1">State</td>
+                        <td>
+                                                    </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Pincode</td><td>401105</td>
+                        <td class="column1">Pincode</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Landline No.</td>
+                        <td>
+                            0                  
+                        </td>
+                        <td class="column1">Mobile No1.</td><td>8898070157</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">E-mail</td><td>shahkrishna@gmail.com</td>
+                        <td class="column1">Mobile No2.</td><td>9323278270</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Nationality</td><td>INDIAN</td>
+
+                        <td class="column1">Religion</td>
+                        <td>
+                            Hindu                        </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Category</td>
+                        <td>
+                            OPEN
+                        </td>
+                        <td class="column1">Mother tongue</td>
+                        <td>
+                            Hindi                        </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">LDAP User ID</td><td>174101228</td>
+                        <td class="column1">Sub Caste</td><td> </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Discipline</td>
+                        <td>ENGINEERING AND TECHNOLOGY                        </td>
+                        <td class="column1">Status</td>
+                        <td>
+                                                    </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Joining Date</td>
+                        <td>
+                            22 - 
+                            August - 
+                            2017                        </td>
+
+                        <td class="column1">Admission Category</td>
+                        <td>Institute Level                        </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Reserved</td><td>No</td>
+                        <td class="column1">Physically Handicapped</td><td>No </td>
+                    </tr>
+                     <tr>
+                <td class="column1"><b>Mentor:</b></td><td colspan="3"><b>Mr. Alex</b></td>
+                     </tr>
+                </tbody></table>
+            </center>
+            <br>
+            <center>
+                <table>
+                                                   <thead>
+                        <tr class="odd">
+                            <th scope="col" colspan="3">Academic Details</th>
+                        </tr>
+                    </thead>
+                    <tbody><tr>
+                        <td class="column1"></td>
+                        <td class="column1"><b>Std. X</b></td>
+                        <td class="column1"><b>Std. XII</b></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Board</td>
+                        <td>SSC</td><td>HSC</td>
+                    </tr>
+
+                    <tr>
+                        <td class="column1">Name of School/College and Board </td>
+                        <td>MAA BHARATI HINDI HIGH SCHOOL</td><td></td>
+                    </tr>
+
+                    <tr>
+                        <td class="column1">Medium of Instruction</td>
+                        <td>English                        </td><td>English</td>
+                    </tr>
+
+                    <tr>
+                        <td class="column1">Maths Marks</td>
+                        <td>85 </td><td>59 </td>
+                    </tr>
+
+                    <tr>
+                        <td class="column1">Science Marks</td>
+                        <td>73</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Physics Marks</td>
+                        <td></td><td>56</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Chemistry Marks</td>
+                        <td></td><td>69</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">PCM</td>
+                        <td></td><td>184</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Aggregate</td>
+                        <td>409.00</td><td>407.00</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Percentage</td>
+                        <td>81.80</td><td>62.62</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Month &amp; Year of Passing</td>
+                        <td>
+                            March 
+                                                    </td>
+                        <td>
+                            March 
+                                                    </td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Class</td>
+                        <td></td><td></td>
+                    </tr>
+                                            <tr>
+                            <td class="column1"></td>
+                            <td class="column1" colspan="2"><b>MHT-CET</b></td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Merit No.</td>
+                            <td colspan="2">0</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Physics Marks</td>
+                            <td colspan="2">8.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Chemistry Marks</td>
+                            <td colspan="2">15.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Maths Marks</td>
+                            <td colspan="2">28.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">PCM</td>
+                            <td colspan="2">51.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Aggregate</td>
+                            <td colspan="2">51.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Percentage</td>
+                            <td colspan="2">0.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Month &amp; Year of Passing</td>
+                            <td colspan="2">
+                                April 
+                                                            </td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Class</td>
+                            <td colspan="2"></td>
+                        </tr>
+
+                                                                <tr>
+                            <td class="column1"></td>
+                            <td class="column1" colspan="2"><b>AIEEE</b></td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Merit No.</td>
+                            <td colspan="2">0</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Physics Marks</td>
+                            <td colspan="2">16.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Chemistry Marks</td>
+                            <td colspan="2">2.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Maths Marks</td>
+                            <td colspan="2">13.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">PCM</td>
+                            <td colspan="2">31.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Aggregate</td>
+                            <td colspan="2">31.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Percentage</td>
+                            <td colspan="2">0.00</td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Month &amp; Year of Passing</td>
+                            <td colspan="2">
+                                May 
+                                                            </td>
+                        </tr>
+                        <tr>
+                            <td class="column1">Class</td>
+                            <td colspan="2"></td>
+                        </tr>
+
+                                                        </tbody></table>
+            </center>
+            <br>
+            <center>
+                <table>
+                    <thead>
+                        <tr class="odd">
+                            <th scope="col" colspan="4">Parent Details</th>
+                        </tr>
+                    </thead>
+                    <tbody><tr>
+                        <td class="column1" colspan="2"><b>Father Details</b></td><td class="column1" colspan="2"><b>Mother Details</b></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Name</td><td>Satish</td>
+                        <td class="column1">Name</td><td>Munni</td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Education</td><td></td>
+                        <td class="column1">Education</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Occupation</td><td></td>
+                        <td class="column1">Occupation</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Office Address</td><td></td>
+                        <td class="column1">Office Address</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Office Land Line</td><td></td>
+                        <td class="column1">Office Land Line</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Mobile No.</td><td></td>
+                        <td class="column1">Mobile No.</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Email</td><td></td>
+                        <td class="column1">Email</td><td></td>
+                    </tr>
+                    <tr>
+                        <td class="column1">Income</td><td></td>
+                        <td class="column1">Income</td><td></td>
+                    </tr>
+                </tbody></table>
+            </center>
+            <br>
+            				<center>
+				<table>
+                <thead>
+                    <tr class="odd">
+                        <th colspan="7" scope="col"> Engineering Academic Marks Details</th>
+                    </tr>
+                </thead>                                                    
+                <tbody><tr>
+                    <td class="column1">Semester</td>
+                    <td class="column1">Seat No</td>
+                    <td class="column1">Month</td>
+                    <td class="column1">Year</td>
+                    <td class="column1">Sum CGP</td>
+                    <td class="column1">SGPI</td>
+                    <td class="column1">Result</td>
+                    
+                </tr>
+    
+				<tr><td>1</td><td>13118582</td><td>Dec</td><td>2017</td><td>207.5</td><td>7.69</td><td>P</td></tr><tr><td>2</td><td>3114594</td><td>May</td><td>2019</td><td>188.5</td><td>6.98</td><td>P</td></tr><tr><td>3</td><td>4370</td><td>Dec</td><td>2018</td><td>191</td><td>7.35</td><td>P</td></tr><tr><td>4</td><td>44070</td><td>May</td><td>2019</td><td>189</td><td>7.27</td><td>P</td></tr></tbody></table></center>
+</body></html>
---
+++ ip/report/report.php
@@ -0,0 +1,143 @@
+<html><head>
+        <title>VCET Attendance Manager</title>
+        <!-- <link rel="stylesheet" href="index1.css" type="text/class"> -->
+
+     <link rel="stylesheet" href="css/menu_style.css" type="text/css">
+       <script language="javascript" type="text/javascript">
+            function confirmation() {
+                var answer = confirm("Are you sure you want to Logout ?")
+                if (answer){
+                    window.location = "logout.php";
+                }
+            }
+        </script>
+        
+  
+</head>
+        
+        <title>Student Attendance</title>
+    
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/table1.css" rel="stylesheet" type="text/css">
+
+
+                <center><h3>Attendance Result</h3></center>
+            <table style="margin: auto;">
+                <tbody><tr>
+                    <td colspan="4">Student Name : Shah Balkrishna A</td>
+                </tr>
+                <tr>
+                    <th>Subject</th>
+                    <th>Total Lectures</th>
+                    <th>Lectures Present</th>
+                    <th>(%)</th>
+                </tr>
+                                    <tr>
+                        <td> Centre for soft skill &amp; personality development  </td>
+                        <td align="center"> 4 </td>
+                        <td align="center"> 4 </td>
+                        <td align="center">
+                            100                    </td>
+                    </tr>
+
+                                    <tr>
+                        <td> Microcontroller and Embedded Programming </td>
+                        <td align="center"> 52 </td>
+                        <td align="center"> 46 </td>
+                        <td align="center">
+                            88.46                    </td>
+                    </tr>
+
+                                    <tr>
+                        <td> Internet Programming </td>
+                        <td align="center"> 55 </td>
+                        <td align="center"> 50 </td>
+                        <td align="center">
+                            90.91                    </td>
+                    </tr>
+
+                                    <tr>
+                        <td> Advanced Data Management Technology </td>
+                        <td align="center"> 52 </td>
+                        <td align="center"> 47 </td>
+                        <td align="center">
+                            90.38                    </td>
+                    </tr>
+
+                                    <tr>
+                        <td> Cryptography &amp; Network Security </td>
+                        <td align="center"> 54 </td>
+                        <td align="center"> 46 </td>
+                        <td align="center">
+                            85.19                    </td>
+                    </tr>
+
+                                    <tr>
+                        <td> Advanced Data Structures &amp; Analysis of Algorithms </td>
+                        <td align="center"> 52 </td>
+                        <td align="center"> 47 </td>
+                        <td align="center">
+                            90.38                    </td>
+                    </tr>
+
+                                    <tr>
+                        <td> Business Communication and Ethics </td>
+                        <td align="center"> 26 </td>
+                        <td align="center"> 22 </td>
+                        <td align="center">
+                            84.62                    </td>
+                    </tr>
+
+                                    <tr>
+                    <td style="text-align: right;"><b>Total</b></td>
+                    <td style="text-align: center;"><b>295</b></td>
+                    <td style="text-align: center;"><b>262</b></td>
+                </tr>
+            </tbody></table>
+        <br>
+        <center> <h2>Overall Attendance: <u>88.81</u> </h2> </center>            <br><br>       
+            <table style="margin: auto;">                
+                <tbody><tr>
+                    <th>Subject</th>
+                    <th>Total Practicals Conducted</th>
+                    <th>Practicals Attended</th>
+                    <th>(%)</th>
+                </tr>
+                                    <tr>
+                        <td> Business Communication and Ethics </td>
+                        <td style="text-align: center;"> 10 </td>
+                        <td style="text-align: center;"> 8 </td>
+                        <td style="text-align: center;">
+                            80                        </td>
+                    </tr>
+                                    <tr>
+                        <td> Internet Programming Lab </td>
+                        <td style="text-align: center;"> 9 </td>
+                        <td style="text-align: center;"> 8 </td>
+                        <td style="text-align: center;">
+                            88.89                        </td>
+                    </tr>
+                                    <tr>
+                        <td> IOT (Mini Project) Lab </td>
+                        <td style="text-align: center;"> 12 </td>
+                        <td style="text-align: center;"> 12 </td>
+                        <td style="text-align: center;">
+                            100                        </td>
+                    </tr>
+                                    <tr>
+                        <td> OLAP Lab </td>
+                        <td style="text-align: center;"> 11 </td>
+                        <td style="text-align: center;"> 11 </td>
+                        <td style="text-align: center;">
+                            100                        </td>
+                    </tr>
+                                    <tr>
+                        <td> Security Lab </td>
+                        <td style="text-align: center;"> 10 </td>
+                        <td style="text-align: center;"> 10 </td>
+                        <td style="text-align: center;">
+                            100                        </td>
+                    </tr>
+                            </tbody></table>
+            <label style="text-align: center;"><h2>Overall Practical Attendance: 94.23%</h2></label>
+</body></html>
